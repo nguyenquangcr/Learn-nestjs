0f802b3d3488a4e137a152fa03c9f9e8bed40534
Quang Trần <quang.tran@Alta.com.vn>
2023-04-06 18:52:25 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors = require('cors');
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   app.useGlobalPipes(
     new ValidationPipe(),
     // {forbidUnknownValues: false}
